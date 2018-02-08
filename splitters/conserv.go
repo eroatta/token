8316761a4dbd8457458d932d981f46c96162fabf
eroatta <emilianoroatta@gmail.com>
2018-02-07 23:55:34 -0300

    added some regexp for conservative splitting

--- splitters/conserv.go
+++ splitters/conserv.go
@@ -1,5 +1,9 @@
 package splitters
 
+import (
+	"regexp"
+)
+
 // Conserv represents a conservative splitter.
 type Conserv struct {
 	Splitter
@@ -7,5 +11,16 @@ type Conserv struct {
 
 // Split on Conserv receives a token and returns an array of hard-words.
 func (c Conserv) Split(token string) ([]string, error) {
-	return nil, nil
+	//remove numbers
+	numRegex := regexp.MustCompile("([0-9]+)")
+	t := numRegex.ReplaceAllString(token, "_${1}_")
+
+	//separate camelcase through underscores
+	ccRegex := regexp.MustCompile("([a-z])([A-Z])")
+	cc := ccRegex.ReplaceAllString(t, "${1}_$2")
+
+	regex := regexp.MustCompile("_")
+	s := regex.Split(cc, -1)
+
+	return s, nil
 }
