cdc35d7ca3d02550b41c065c795c65cb2ee80e5a
eroatta <emilianoroatta@gmail.com>
2018-02-12 17:34:03 -0300

    refactored conserv splitter using common functions

--- splitters/conserv.go
+++ splitters/conserv.go
@@ -15,21 +15,9 @@ type Conserv struct {
 // * Numbers
 // * CamelCase.
 func (c Conserv) Split(token string) ([]string, error) {
-	//add markers between letters and numbers
-	leadingNumRegex := regexp.MustCompile("([a-zA-Z])([0-9])")
-	processedToken := leadingNumRegex.ReplaceAllString(token, "${1}_$2")
-
-	//add markers between numbers and letters
-	trailingNumRegex := regexp.MustCompile("([0-9])([a-zA-Z])")
-	processedToken = trailingNumRegex.ReplaceAllString(processedToken, "${1}_$2")
-
-	//add markers for lower to upper case camel-case combination
-	lowerToUpperRegex := regexp.MustCompile("([a-z])([A-Z])")
-	processedToken = lowerToUpperRegex.ReplaceAllString(processedToken, "${1}_$2")
-
-	//add markers for upper to lower case camel-case combination
-	upperToLowerRegex := regexp.MustCompile("([A-Z]+)([A-Z])([a-z])")
-	processedToken = upperToLowerRegex.ReplaceAllString(processedToken, "${1}_$2$3")
+	processedToken := addMarkersOnDigits(token)
+	processedToken = addMarkersOnLowerToUpperCase(processedToken)
+	processedToken = addMarkersOnUpperToLowerCase(processedToken)
 
 	regex := regexp.MustCompile("_")
 	splitToken := regex.Split(processedToken, -1)
--- splitters/splitter.go
+++ splitters/splitter.go
@@ -1,6 +1,36 @@
 package splitters
 
+import "regexp"
+
 // Splitter defines the required behavior for any token splitter.
 type Splitter interface {
 	Split(token string) ([]string, error)
+}
+
+func addMarkersOnDigits(token string) string {
+	//add markers between letters and numbers
+	leadingNumRegex := regexp.MustCompile("([a-zA-Z])([0-9])")
+	processedToken := leadingNumRegex.ReplaceAllString(token, "${1}_$2")
+
+	//add markers between numbers and letters
+	trailingNumRegex := regexp.MustCompile("([0-9])([a-zA-Z])")
+	processedToken = trailingNumRegex.ReplaceAllString(processedToken, "${1}_$2")
+
+	return processedToken
+}
+
+func addMarkersOnLowerToUpperCase(token string) string {
+	//add markers for lower to upper case camel-case combination
+	lowerToUpperRegex := regexp.MustCompile("([a-z])([A-Z])")
+	processedToken := lowerToUpperRegex.ReplaceAllString(token, "${1}_$2")
+
+	return processedToken
+}
+
+func addMarkersOnUpperToLowerCase(token string) string {
+	//add markers for upper to lower case camel-case combination
+	upperToLowerRegex := regexp.MustCompile("([A-Z]+)([A-Z])([a-z])")
+	processedToken := upperToLowerRegex.ReplaceAllString(token, "${1}_$2$3")
+
+	return processedToken
 }
