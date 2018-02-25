0d85f0a51a10e23e0dc516ff958502461cbf90bb
eroatta <emilianoroatta@gmail.com>
2018-02-25 18:59:07 -0300

    added mixedCaseSplit on samurai

--- splitters/samurai.go
+++ splitters/samurai.go
@@ -1,5 +1,10 @@
 package splitters
 
+import (
+	"math"
+	"regexp"
+)
+
 type frequencyTable map[string]float32
 
 var defaultLocalFreqTable frequencyTable
@@ -7,6 +12,8 @@ var defaultGlobalFreqTable frequencyTable
 
 // Samurai represents the Samurai splitting algorithm, proposed by Hill et all.
 type Samurai struct {
+	Splitter
+
 	localFreqTable  *frequencyTable
 	globalFreqTable *frequencyTable
 }
@@ -14,18 +21,14 @@ type Samurai struct {
 // NewSamurai creates a new Samurai splitter with the provided frequency tables. If no frequency
 // tables are provided, the default tables are used.
 func NewSamurai(localFreqTable *frequencyTable, globalFreqTable *frequencyTable) *Samurai {
-	var local *frequencyTable
+	local := &defaultLocalFreqTable
 	if localFreqTable != nil {
 		local = localFreqTable
-	} else {
-		local = &defaultLocalFreqTable
 	}
 
-	var global *frequencyTable
+	global := &defaultGlobalFreqTable
 	if globalFreqTable != nil {
 		global = globalFreqTable
-	} else {
-		global = &defaultGlobalFreqTable
 	}
 
 	return &Samurai{
@@ -37,5 +40,51 @@ func NewSamurai(localFreqTable *frequencyTable, globalFreqTable *frequencyTable)
 // Split on Samurai receives a token and returns an array of hard/soft words,
 // split by the Samurai algorithm proposed by Hill et all.
 func (s *Samurai) Split(token string) ([]string, error) {
-	return []string{token}, nil
+	preprocessedToken := addMarkersOnDigits(token)
+	preprocessedToken = addMarkersOnLowerToUpperCase(preprocessedToken)
+
+	reg := regexp.MustCompile("[A-Z][a-z]")
+
+	var processedToken string
+	for _, word := range splitOnMarkers(preprocessedToken) {
+		cutLocation := reg.FindStringIndex(word)
+		if len(word) > 1 && cutLocation != nil {
+			n := len(word) - 1
+
+			i := cutLocation[0]
+			var camelScore float64
+			if i > 0 {
+				camelScore = s.score(word[i:n])
+			} else {
+				camelScore = s.score(word[0:n])
+			}
+
+			altCamelScore := s.score(word[i+1 : n])
+			if camelScore > math.Sqrt(altCamelScore) {
+				if i > 0 {
+					word = word[0:i-1] + "_" + word[i:n]
+				}
+			} else {
+				word = word[0:i] + "_" + word[i+1:n]
+			}
+		}
+
+		processedToken = processedToken + "_" + word
+	}
+
+	splitToken := make([]string, 0, 10)
+	for _, word := range splitOnMarkers(preprocessedToken) {
+		sameCaseSplitting := s.sameCaseSplit(word, s.score(word))
+		splitToken = append(splitToken, sameCaseSplitting...)
+	}
+
+	return splitToken, nil
+}
+
+func (s *Samurai) sameCaseSplit(token string, score float64) []string {
+	return []string{}
+}
+
+func (s *Samurai) score(word string) float64 {
+	return 0.0
 }
