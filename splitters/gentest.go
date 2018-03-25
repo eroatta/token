4673782a2961a7b3bf56221ef36681ba3d40500c
eroatta <emilianoroatta@gmail.com>
2018-03-25 10:27:41 -0300

    added generation task for GenTest

--- splitters/gentest.go
+++ splitters/gentest.go
@@ -1,5 +1,9 @@
 package splitters
 
+import (
+	"sync"
+)
+
 // GenTest represents a Generation and Tests splitting algorithm, proposed by Lawrie, Binkley and Morrell.
 type GenTest struct {
 }
@@ -12,5 +16,54 @@ func NewGenTest() *GenTest {
 // Split on GenTest receives a token and returns an array of hard/soft words,
 // split by the Generation and Test algorithm proposed by Lawrie, Binkley and Morrell.
 func (g *GenTest) Split(token string) ([]string, error) {
-	return []string{}, nil
+	preprocessedToken := addMarkersOnDigits(token)
+	preprocessedToken = addMarkersOnLowerToUpperCase(preprocessedToken)
+
+	splitToken := make([]string, 0, 10)
+	for _, word := range splitOnMarkers(preprocessedToken) {
+		splitToken = append(splitToken, word)
+	}
+
+	return splitToken, nil
+}
+
+func (g *GenTest) generate(token string) []string {
+	var selectedSplit []string
+
+	wg := new(sync.WaitGroup)
+
+	potentialSplits := make(chan []string)
+
+	size := len(token)
+	for i := 0; i < len(token); i++ {
+		wg.Add(1)
+		go func(i int, n int, token string, wg *sync.WaitGroup, splits *chan []string) {
+			defer wg.Done()
+			*splits <- []string{token[0:i], token[i:n]}
+		}(i, size, token, wg, &potentialSplits)
+	}
+	wg.Wait()
+
+	close(potentialSplits)
+
+	return selectedSplit
+}
+
+// generate creates every possible splitting for a given token.
+func generate(token string) []string {
+	splits := []string{token}
+	for i := 1; i < len(token); i++ {
+		leading := token[:i]
+		trailing := token[i:]
+
+		split := leading + "_" + trailing
+		splits = append(splits, split)
+
+		for j := 1; j < len(trailing); j++ {
+			split = leading + "_" + trailing[:j] + "_" + trailing[j:]
+			splits = append(splits, split)
+		}
+	}
+
+	return splits
 }
--- splitters/gentest_test.go
+++ splitters/gentest_test.go
@@ -1,1 +1,87 @@
 package splitters
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenTestSplitting(t *testing.T) {
+	cases := []struct {
+		ID       int
+		token    string
+		expected []string
+	}{
+		{0, "car", []string{"car"}},
+		{1, "getString", []string{"get", "String"}},
+		{2, "GPSstate", []string{"GPS", "state"}},
+		{3, "ASTVisitor", []string{"AST", "Visitor"}},
+		{4, "notype", []string{"no", "type"}},
+	}
+
+	gentest := NewGenTest()
+	for _, c := range cases {
+		got, err := gentest.Split(c.token)
+		if err != nil {
+			assert.Fail(t, "we shouldn't get any errors at this point", err)
+		}
+
+		assert.Equal(t, c.expected, got, "elements should match in number and order for identifier number")
+	}
+}
+
+func BenchmarkGenTestSplitting(b *testing.B) {
+	benchmarks := []struct {
+		name  string
+		token string
+	}{
+		{"Short token", "car"},
+		{"Medium token", "numsize"},
+		{"Long token", "allocatedsize"},
+		{"Longest token", "veryverylongtokennameforsplitting"},
+	}
+
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				generate(bm.token)
+			}
+		})
+	}
+}
+
+func TestGenerateSplits(t *testing.T) {
+	cases := []struct {
+		token    string
+		expected []string
+	}{
+		{"car", []string{"car", "c_ar", "c_a_r", "ca_r"}},
+		{"bond", []string{"bond", "b_ond", "b_o_nd", "b_on_d", "bo_nd", "bo_n_d", "bon_d"}},
+	}
+
+	for _, c := range cases {
+		got := generate(c.token)
+
+		assert.ElementsMatch(t, c.expected, got, "elements should match")
+	}
+}
+
+func BenchmarkGenerate(b *testing.B) {
+	benchmarks := []struct {
+		name  string
+		token string
+	}{
+		{"Short token", "car"},
+		{"Medium token", "numsize"},
+		{"Long token", "allocatedsize"},
+		{"Longest token", "veryverylongtokennameforsplitting"},
+	}
+
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				generate(bm.token)
+			}
+		})
+	}
+}
