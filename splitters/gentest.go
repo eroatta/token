7bd0c7bf393fd1df2a5483c78c113c3e6cf95ba1
eroatta <emilianoroatta@gmail.com>
2019-01-27 19:20:07 -0300

    added pseudo code for GenTest and functions for getting expansions/translations

--- splitters/gentest.go
+++ splitters/gentest.go
@@ -1,16 +1,42 @@
 package splitters
 
 import (
-	"sync"
+	"fmt"
+	"regexp"
+	"strings"
 )
 
 // GenTest represents a Generation and Tests splitting algorithm, proposed by Lawrie, Binkley and Morrell.
 type GenTest struct {
+	// TODO review
+	list string
 }
 
 // NewGenTest creates a new GenTest splitter.
 func NewGenTest() *GenTest {
 	return &GenTest{}
+}
+
+// PotentialSplit represents a GenTest potential split. It holds data related to the split, the softwords
+// and their expansions.
+type PotentialSplit struct {
+	split      string
+	softwords  []string
+	expansions map[string][]string
+}
+
+// NewPotentialSplit creates and initializes a new potential split for the given hardword.
+func NewPotentialSplit(hardword string) PotentialSplit {
+	var softwords []string
+	if hardword != "" {
+		softwords = splitOnMarkers(hardword)
+	}
+
+	return PotentialSplit{
+		split:      hardword,
+		softwords:  softwords,
+		expansions: make(map[string][]string, len(softwords)),
+	}
 }
 
 // Split on GenTest receives a token and returns an array of hard/soft words,
@@ -24,48 +50,74 @@ func (g *GenTest) Split(token string) ([]string, error) {
 		splitToken = append(splitToken, word)
 	}
 
+	for _, tok := range splitToken {
+		for _, pSplit := range generatePotentialSplits(tok) {
+			fmt.Sprintln(pSplit.split)
+		}
+	}
+
+	// generate potential splitings
+	// e.g. splits("strlen") = {st-rlen, str-len}
+	// find the list of expansions/translations for each potential splitting
+	// e.g. E(st) = {stop, string, set}
+	// e.g. E(rlen) = {riflemen}
+	// e.g. E(str) = {steer, string}
+	// e.g. E(len) = {lender, length}
+	// define the similarity score between an expansion and the other soft words on the potential splittings
+	// list, but for the same word (k not i)
+	// uses the Google Data Set, and sim(Ei,j; Sk) = sum sim(Ei,j, e) <- e = E(Sk)
+	// e.g. sim(string, len) = sim(string, lender) + sim(string, length)
+
+	// computation for cohesion sums over all the other soft-words besides the current one
+	// e.g. cohesion(string, str-len) = log(sim(string, len))
+
+	// the maximal cohesion is chosen among the possible expansions
+	// e.g. cohesion(string, str-len) = -5.9431
+	// e.g. cohesion(steer, str-len) = -16.1222
+
+	// score is the maximal cohesion with the selected translation as default expansion
+	// e.g. score(str) = -5.9431 with "string" as expansion
+
 	return splitToken, nil
 }
 
-func (g *GenTest) generate(token string) []string {
-	var selectedSplit []string
-
-	wg := new(sync.WaitGroup)
-
-	potentialSplits := make(chan []string)
-
-	size := len(token)
-	for i := 0; i < len(token); i++ {
-		wg.Add(1)
-		go func(i int, n int, token string, wg *sync.WaitGroup, splits *chan []string) {
-			defer wg.Done()
-			*splits <- []string{token[0:i], token[i:n]}
-		}(i, size, token, wg, &potentialSplits)
-	}
-	wg.Wait()
-
-	close(potentialSplits)
-
-	return selectedSplit
-}
-
-// generateSplits creates every possible splitting for a given token.
-func generateSplits(token string) []string {
-	splits := []string{token}
+// generatePotentialSplits generates every possible splitting for a given token.
+func generatePotentialSplits(token string) []PotentialSplit {
+	potentialSplits := []PotentialSplit{NewPotentialSplit(token)}
 	for i := 1; i < len(token); i++ {
 		leading := token[:i]
 		trailing := token[i:]
 
-		split := leading + "_" + trailing
-		splits = append(splits, split)
+		potentialSplit := leading + "_" + trailing
+		potentialSplits = append(potentialSplits, NewPotentialSplit(potentialSplit))
 
 		for j := 1; j < len(trailing); j++ {
-			split = leading + "_" + trailing[:j] + "_" + trailing[j:]
-			splits = append(splits, split)
+			potentialSplit = leading + "_" + trailing[:j] + "_" + trailing[j:]
+			potentialSplits = append(potentialSplits, NewPotentialSplit(potentialSplit))
 		}
 	}
 
-	return splits
+	return potentialSplits
+}
+
+// findExpansions retrieves a set of words that could be considered expansions for the input string.
+func (g *GenTest) findExpansions(input string) []string {
+	if input == "" || strings.TrimSpace(input) == "" {
+		return []string{}
+	}
+
+	// build the regexp
+	var pattern strings.Builder
+	pattern.WriteString("\\b")
+	for _, char := range input {
+		pattern.WriteString("[")
+		pattern.WriteRune(char)
+		pattern.WriteString("]\\w*")
+	}
+	exp := regexp.MustCompile(pattern.String())
+
+	// find on a list (what should this list include?)
+	return exp.FindAllString(g.list, -1)
 }
 
 func testSplit(split string) {
--- splitters/gentest_test.go
+++ splitters/gentest_test.go
@@ -1,12 +1,13 @@
 package splitters
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGenTestSplitting(t *testing.T) {
+func TestSplit_OnGenTest_ShouldReturnValidSplits(t *testing.T) {
 	cases := []struct {
 		ID       int
 		token    string
@@ -30,27 +31,7 @@ func TestGenTestSplitting(t *testing.T) {
 	}
 }
 
-func BenchmarkGenTestSplitting(b *testing.B) {
-	benchmarks := []struct {
-		name  string
-		token string
-	}{
-		{"Short token", "car"},
-		{"Medium token", "numsize"},
-		{"Long token", "allocatedsize"},
-		{"Longest token", "veryverylongtokennameforsplitting"},
-	}
-
-	for _, bm := range benchmarks {
-		b.Run(bm.name, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				generateSplits(bm.token)
-			}
-		})
-	}
-}
-
-func TestGenerateSplits(t *testing.T) {
+func TestGeneratePotentialSplits_ShouldReturnEveryPossibleCombination(t *testing.T) {
 	cases := []struct {
 		token    string
 		expected []string
@@ -60,10 +41,54 @@ func TestGenerateSplits(t *testing.T) {
 	}
 
 	for _, c := range cases {
-		got := generateSplits(c.token)
+		var got []string
+		for _, potentialSplit := range generatePotentialSplits(c.token) {
+			got = append(got, potentialSplit.split)
+		}
 
 		assert.ElementsMatch(t, c.expected, got, "elements should match")
 	}
+}
+
+func TestFindExpansions_OnGenTestWithCustomList_ShouldReturnAllMatches(t *testing.T) {
+	tests := []struct {
+		name       string
+		input      string
+		expansions []string
+	}{
+		{"input_st", "st", []string{"string", "steer", "set"}},
+		{"input_rlen", "rlen", []string{"riflemen"}},
+		{"input_str", "str", []string{"steer", "string"}},
+		{"input_len", "len", []string{"lender", "length"}},
+		{"empty_input", "", []string{}},
+		{"blankspace_input", " ", []string{}},
+	}
+
+	gentest := NewGenTest()
+	gentest.list = "car string steer set riflemen lender bar length kamikaze"
+	for _, fixture := range tests {
+		t.Run(fixture.name, func(t *testing.T) {
+			got := gentest.findExpansions(fixture.input)
+
+			assert.ElementsMatch(t, fixture.expansions, got, fmt.Sprintf("found elements: %v", got))
+		})
+	}
+}
+
+func TestNewPotentialSplit_OnMarkedHardword_ShouldReturnPotentialSplit(t *testing.T) {
+	got := NewPotentialSplit("foo_bar")
+
+	assert.Equal(t, "foo_bar", got.split, "split should match de input")
+	assert.ElementsMatch(t, []string{"foo", "bar"}, got.softwords, "elements should match")
+	assert.Equal(t, 0, len(got.expansions), "expansions map should be empty")
+}
+
+func TestNewPotentialSplit_OnEmptyHardword_ShouldReturnEmptyPotentialSplit(t *testing.T) {
+	got := NewPotentialSplit("")
+
+	assert.Equal(t, "", got.split, "split should be empty")
+	assert.ElementsMatch(t, []string{}, got.softwords, "there should be no softwords")
+	assert.Equal(t, 0, len(got.expansions), "there should be no elements")
 }
 
 func BenchmarkGenerate(b *testing.B) {
@@ -80,7 +105,7 @@ func BenchmarkGenerate(b *testing.B) {
 	for _, bm := range benchmarks {
 		b.Run(bm.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				generateSplits(bm.token)
+				generatePotentialSplits(bm.token)
 			}
 		})
 	}
