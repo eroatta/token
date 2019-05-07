91e4bf3ca8e5884525bf9c9655a61fdcbab00e28
eroatta <emilianoroatta@gmail.com>
2019-05-07 20:15:59 -0300

    reordered test cases

--- expanders/basic_test.go
+++ expanders/basic_test.go
@@ -27,10 +27,10 @@ func TestExpand_OnBasic_ShouldReturnExpansion(t *testing.T) {
 		expected []string
 	}{
 		{"no_expansion", "noExpansion", []string{}},
+		{"in_words", "parser", []string{"parser"}},
+		{"phrase", "JSON", []string{"java", "script", "object", "notation"}},
 		{"in_lower_case", "case", []string{"case"}},
 		{"case_unsensitive", "Case", []string{"case"}},
-		{"phrase", "JSON", []string{"java", "script", "object", "notation"}},
-		{"in_words", "parser", []string{"parser"}},
 	}
 
 	words := map[string]interface{}{
