5ef540c2b29b8f70c497acb3d3dced207687da8b
eroatta <emilianoroatta@gmail.com>
2019-05-25 18:01:31 -0300

    added more documentation to AMAP

--- expanders/amap.go
+++ expanders/amap.go
@@ -1,6 +1,6 @@
 package expanders
 
-// Amap represents an Au
+// Amap represents an Automatically Mining Abbreviations in Programs expander.
 type Amap struct {
 }
 
@@ -9,7 +9,12 @@ func NewAmap() *Amap {
 	return &Amap{}
 }
 
-// Expand TODO
+// Expand on AMAP receives a token and returns and array of possible expansions.
+//
+// The AMAP expansion algorithm handles single-word and multi-word abbreviations.
+// For each type of abbreviation AMAP creates and applies a pattern to look for possible
+// expansions. AMAP is capable of select the more appropiate expansions based on available
+// information on the given context.
 func (a Amap) Expand(token string) []string {
 	return []string{}
 }
