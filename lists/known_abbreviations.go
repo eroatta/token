6acae99d31c4ee117235ac5629fb262995983636
eroatta <emilianoroatta@gmail.com>
2019-07-04 09:22:18 -0300

    removed old way to create lists

---
+++ lists/known_abbreviations.go
@@ -0,0 +1,8 @@
+package lists
+
+// KnownAbbreviations is a list of strings that are known and common abbreviations on the language.
+var KnownAbbreviations []string
+
+func init() {
+	KnownAbbreviations = []string{}
+}
