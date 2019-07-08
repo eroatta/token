f3843fafb6b49d18251faeeaca59ef1674c89dc3
eroatta <emilianoroatta@gmail.com>
2019-07-08 13:02:26 -0300

    removed unnecesary pointer definitions and reordered code

--- samurai/frequency_table.go
+++ samurai/frequency_table.go
@@ -5,6 +5,10 @@ import (
 	"strings"
 )
 
+// errOccurrencesGreatherOrEqualThanZero indicates that occurrences on a frequency table must be always
+// greater or equal than zero.
+var errOccurrencesGreatherOrEqualThanZero = errors.New("Occurrences must be greater or equal than 0")
+
 // FrequencyTable is a lookup table that stores the number of occurrences
 // of each unique string in a set of strings.
 type FrequencyTable struct {
@@ -20,19 +24,10 @@ func NewFrequencyTable() *FrequencyTable {
 	}
 }
 
-// Frequency determines how frequently a token occurs in a set of strings.
-func (f *FrequencyTable) Frequency(token string) float64 {
-	if f.totalOccurrences == 0 {
-		return 0.0
-	}
-
-	return float64(f.occurrences[strings.ToLower(token)]) / float64(f.totalOccurrences)
-}
-
 // SetOccurrences sets how many times a token appeared in a set of strings.
 func (f *FrequencyTable) SetOccurrences(token string, occurrences int) error {
 	if occurrences < 0 {
-		return errors.New("Occurrences must be greater or equal than 0")
+		return errOccurrencesGreatherOrEqualThanZero
 	}
 
 	key := strings.ToLower(token)
@@ -43,6 +38,15 @@ func (f *FrequencyTable) SetOccurrences(token string, occurrences int) error {
 }
 
 // TotalOccurrences provides the total number of occurrences on the frequency table.
-func (f *FrequencyTable) TotalOccurrences() int {
+func (f FrequencyTable) TotalOccurrences() int {
 	return f.totalOccurrences
+}
+
+// Frequency determines how frequently a token occurs in a set of strings.
+func (f FrequencyTable) Frequency(token string) float64 {
+	if f.totalOccurrences == 0 {
+		return 0.0
+	}
+
+	return float64(f.occurrences[strings.ToLower(token)]) / float64(f.totalOccurrences)
 }
